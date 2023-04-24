a232a44016b04b180cdffea31280a4d076e47819
furkantunc3r <furkan_tuncer@yahoo.com>
2023-04-24 15:09:51 +0300

    line

--- methods/srcs/ping.cpp
+++ methods/srcs/ping.cpp
@@ -9,7 +9,5 @@ void Ping::execute(std::vector<std::string>& args, int fd)
     std::string msg;
     
     msg.append(":localhost PONG localhost "+ this->_server.get_user(fd)._nickname + "\r\n");
-    std::cout << msg << std::endl;
-
     send(fd, msg.c_str(), msg.size(), 0);
 }
